e0fe82882ec46476a8478195e6ad9e109ce604ca
youssoufmiyad <youssoufmiyad@gmail.com>
2025-10-09 03:39:52 +0200

    page home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div>
-      HOME
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Home
+  return (
+    <div className="home-page">
+      <header>
+        <div className="content-container">
+          <h1>HOME</h1>
+          <p>Welcome to the Home Page!</p>
+          <div className="actions">
+            <button type="button" onClick={() => navigate("/signup")}>Sign up</button>
+            <button type="button" onClick={() => navigate("/login")}>Login</button>
+          </div>
+        </div>
+        
+      </header>
+      <section>
+        <div className="content-container grid-3">
+          <div className="grid-item">
+            <h2>Keep a track of your contacts</h2>
+            <p>Stay organized and never lose track of your important connections. MyContacts is here to help you manage your relationships effectively.</p>
+          </div>
+          <div className="grid-item">
+            <h2>Log informations efficiently</h2>
+            <p>MyContacts allows you to log and manage your contact information with ease.</p>
+          </div>
+          <div className="grid-item">
+            <h2>Privacy Matters</h2>
+            <p>Your privacy is our priority. We ensure that your data is handled with care and respect.</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
